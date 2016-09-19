3d6ba87d5e5ba67f5c522107c065cb53ea11ccde
carol <carolmurithi1994@gmail.com>
2016-09-19 15:45:00 +0300

    converting f to degrees and viceversa

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,5 +1,7 @@
-def ftoc(degrees)
-0#{"degrees"}
-100#{"degrees"}
+def ftoc(f_temp)
+   (f_temp -  32) * 5.0/9.0#converting f to temp
 end
 
+def ctof(c_temp)
+  (c_temp * 9.0/5.0) + 32#converting temp to f
+end
