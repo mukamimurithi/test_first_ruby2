e763dbabe4c15ded46ac3a1d5c05d39da8921229
carol <carolmurithi1994@gmail.com>
2016-09-22 17:30:19 +0300

    learn gsub,capitalizing and  defaul values

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -4,12 +4,22 @@ end
 
 def shout(hello)
   hello.upcase
+end 
+
+def repeat (hello, x = 2)
+([hello] * x) .join (" ")
 end
 
-def repeat(hello)
-  #hello hello
+def start_of_word(hello, a=1)
+hello [0,a]
 end
 
-def hello
-0
+def first_word(word)
+  word.split.first
+end
+def titleize(sentence)
+  little_words = %w(end over and the)
+  sentence.capitalize.gsub(/(\w+)/) do |word|
+    little_words.include?(word) ? word : word.capitalize
+  end
 end
